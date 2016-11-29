551d6afce5bcaa22f6a8ba6f54877b172a7a85be
Tanya Madhani <tmadhani@0587340024.wireless.umich.net>
2016-11-29 13:14:29 -0500

    Commit 7

--- mypygame.py
+++ mypygame.py
@@ -1,10 +1,7 @@
 #TANYA'S BREAKOUT GAME
-
 import pygame
-import random
-import math
-import sys
-pygame.init()
+from pygame import *
+from pygame.sprite import *
 
 #colors for game sprites
 brick_red = (255, 0, 0)
@@ -12,8 +9,10 @@ brick_blue = (0,255,0)
 ball_white = (255,255,255)
 paddle_green = (50, 205, 50)
 background_black = (0,0,0)
-#setting display width/height and caption
+
+
 gameDisplay = pygame.display.set_mode((600,400))
+
 pygame.display.set_caption("Breakout")
 pygame.display.update()
 
@@ -56,24 +55,25 @@ class Paddle(pygame.sprite.Sprite):
 		super().__init__()
 
 class Ball(pygame.sprite.Sprite):
-	hit_count = 0
-	velocity = 4.0
 
-	x = 0.0
-	y = 180.0
-
-	direction = 250
-
-	height = 10
-	width = 10
-
-	self.image = pygame.Surface([self.width, self.height])
-	self.image.fill(ball_white)
-	self.rect = self.image.get_rect()
 
 	def __init__(self, color):
 		super().__init__()
+		hit_count = 0
+		velocity = 4.0
 
+		x = 0.0
+		y = 180.0
+
+		direction = 250
+
+		height = 10
+		width = 10
+
+		self.image = pygame.Surface([self.width, self.height])
+		self.image.fill(ball_white)
+		self.rect = self.image.get_rect()
+	
 	def increment_hit(self):
 		self.hit_count = self.hit_count + 1
 		if self.hit_count % 1 == 0:
@@ -82,9 +82,19 @@ class Ball(pygame.sprite.Sprite):
 
 dems = pygame.sprite.Group()
 reps = pygame.sprite.Group()
-
+two_party_system = pygame.sprite.Group()
+y_pos = 80
 for row in range(3):
 	for column in range(30):
+		blue_parties = Blue_Party(brick_blue, column * (brick_width+2)+1, y_pos)
+		dems.add(blue_parties)
+		two_party_system.add(blue_parties)
+		
+for rown in range(3):
+	for column in range(30):
+		red_parties = Red_Party(brick_red, column * (brick_width+2)+1, y_pos)
+		reps.add(red_parties)
+		two_party_system.add(red_parties)
 
-
+pygame.init()
 
