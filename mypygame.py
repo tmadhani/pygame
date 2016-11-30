618a241ea3da347e4b02646d30f38916c9a43645
Tanya Madhani <tmadhani@Tanyas-MacBook-Air.local>
2016-11-29 23:26:51 -0500

    Commit 7

--- mypygame.py
+++ mypygame.py
@@ -2,7 +2,7 @@
 import pygame
 from pygame import *
 from pygame.sprite import *
-
+pygame.init()
 #colors for game sprites
 brick_red = (255, 0, 0)
 brick_blue = (0,255,0)
@@ -51,50 +51,67 @@ class Blue_Party(pygame.sprite.Sprite):
 		self.rect.y = y
 
 class Paddle(pygame.sprite.Sprite):
-	def __init__(self, color):
+	def __init__(self, color, x, y):
 		super().__init__()
-
-class Ball(pygame.sprite.Sprite):
-
-
-	def __init__(self, color):
-		super().__init__()
-		hit_count = 0
-		velocity = 4.0
-
-		x = 0.0
-		y = 180.0
-
-		direction = 250
-
-		height = 10
-		width = 10
-
-		self.image = pygame.Surface([self.width, self.height])
-		self.image.fill(ball_white)
+		self.image = pygame.Surface([paddle_width, paddle_height])
+		self.image.fill(paddle_green)
 		self.rect = self.image.get_rect()
+		self.screenheight = pygame.display.get_surface().get_height()
+		self.screenwidth = pygame.display.get_surface().get_width()
+		self.rect.x = 0
+		self.rect.y = self.screenheight-self.height
+	def move(self, x, y):
+		p = pygame.mouse.get_pos()
+		self.rect.x = pos[0]
+
+# class Ball(pygame.sprite.Sprite):
+
+
+# 	def __init__(self, color):
+# 		super().__init__()
+# 		hit_count = 0
+# 		velocity = 4.0
+
+# 		x = 0.0
+# 		y = 180.0
+
+# 		direction = 250
+
+# 		height = 10
+# 		width = 10
+
+# 		self.image = pygame.Surface([self.width, self.height])
+# 		self.image.fill(ball_white)
+# 		self.rect = self.image.get_rect()
 	
-	def increment_hit(self):
-		self.hit_count = self.hit_count + 1
-		if self.hit_count % 1 == 0:
-			for ball in self.balls:
-				ball.velocity = ball.velocity + 10
+# 	def increment_hit(self):
+# 		self.hit_count = self.hit_count + 1
+# 		if self.hit_count % 1 == 0:
+# 			for ball in self.balls:
+# 				ball.velocity = ball.velocity + 10
+# #class Game(game):
+# 	#def __init__(self):
+# 		#super().__init__()
 
-dems = pygame.sprite.Group()
-reps = pygame.sprite.Group()
-two_party_system = pygame.sprite.Group()
-y_pos = 80
-for row in range(3):
-	for column in range(30):
-		blue_parties = Blue_Party(brick_blue, column * (brick_width+2)+1, y_pos)
-		dems.add(blue_parties)
-		two_party_system.add(blue_parties)
+
+# dems = pygame.sprite.Group()
+# reps = pygame.sprite.Group()
+# two_party_system = pygame.sprite.Group()
+# y_pos = 80
+# for row in range(3):
+# 	for column in range(30):
+# 		blue_parties = Blue_Party(brick_blue, column * (brick_width+2)+1, y_pos)
+# 		dems.add(blue_parties)
+# 		two_party_system.add(blue_parties)
 		
-for rown in range(3):
-	for column in range(30):
-		red_parties = Red_Party(brick_red, column * (brick_width+2)+1, y_pos)
-		reps.add(red_parties)
-		two_party_system.add(red_parties)
+# # for rown in range(3):
+# # 	for column in range(30):
+# # 		red_parties = Red_Party(brick_red, column * (brick_width+2)+1, y_pos)
+# # 		reps.add(red_parties)
+# # 		two_party_system.add(red_parties)
+# # lost_game = False
+# # while not lost_game:
+# #     two_party_system.draw(gameDisplay)
 
-pygame.init()
+# pygame.init()
 
